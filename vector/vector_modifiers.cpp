bbc7f4d10b1f88ddd2550c8362eb8805b3f7e7a3
Badr Kamel <36001332+badrkamel@users.noreply.github.com>
2022-01-29 15:52:55 +0200

    Update vector_modifiers.cpp

--- vector/vector_modifiers.cpp
+++ vector/vector_modifiers.cpp
@@ -18,16 +18,23 @@ int main()
 
 	// inserts 15 to the last position
 	v.push_back(15);
+
 	int n = v.size();
 	cout << "\nThe last element is: " << v[n - 1];
+	// Accessing last element
+	cout << "\nThe last element is: " << v.back();
+
+	// modifying last element
+	v.back() = 100;
+
+	// prints the vector
+	cout << "\nThe vector elements are: ";
+	for (int &i : v)
+		cout << i << " ";
 
 	// removes last element
 	v.pop_back();
 
-	// prints the vector
-	cout << "\nThe vector elements are: ";
-	for (int i : v)
-		cout << i << " ";
 
 	// inserts 5 at the beginning
 	v.insert(v.begin(), 5);
@@ -45,8 +52,12 @@ int main()
 
 	// Inserts 20 at the end
 	v.emplace_back(20);
-	n = v.size();
-	cout << "\nThe last element is: " << v[n - 1];
+	cout << "\nThe last element is: " << v.back();
+
+	v.insert(v.begin()+2, 200);
+	cout << "\nThe vector elements are: ";
+	for (int &i : v)
+		cout << i << " ";
 
 	// erases the vector
 	v.clear();
@@ -71,11 +82,11 @@ int main()
 	v1.swap(v2);
 
 	cout << "\nAfter Swap \nVector 1: ";
-	for (int i = 0; i < v1.size(); i++)
-		cout << v1[i] << " ";
+	for (int &i : v1)
+		cout << i << " ";
 
 	cout << "\nVector 2: ";
-	for (int i = 0; i < v2.size(); i++)
-		cout << v2[i] << " ";
-    cout << endl;
+	for (int &i : v2)
+		cout << i << " ";
+	cout << endl;
 }
